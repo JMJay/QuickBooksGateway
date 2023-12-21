6119535634578b8748339e136093c4eade5b6cf4
Joseph Jay <joseph.michael.jay@gmail.com>
2023-12-21 10:51:33 -0600

    Initial commit

---
+++ src/main/kotlin/com/bakertilly/quickbooksgateway/QuickBooksGatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.bakertilly.quickbooksgateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuickBooksGatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuickBooksGatewayApplication>(*args)
+}
---
+++ src/test/kotlin/com/bakertilly/quickbooksgateway/QuickBooksGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bakertilly.quickbooksgateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuickBooksGatewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
